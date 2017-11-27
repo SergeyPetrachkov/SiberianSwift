4031fea3eb26ebc75a55adde6dd68b83667ade1f
sergey petrachkov <petrachkovsergey@gmail.com>
2017-11-27 20:13:07 +0700

    add copyable extension

---
+++ SiberianSwift/Classes/Copyable+Siberia.swift
@@ -0,0 +1,19 @@
+//
+//  Copyable+Siberia.swift
+//  SiberianSwift
+//
+//  Created by Sergey Petrachkov on 27/11/2017.
+//  Copyright © 2017 Sergey Petrachkov. All rights reserved.
+//
+
+import Foundation
+
+public protocol Copyable {
+  init(other: Self)
+}
+
+public extension Copyable {
+  public func copy() -> Self {
+    return Self.init(other: self)
+  }
+}
