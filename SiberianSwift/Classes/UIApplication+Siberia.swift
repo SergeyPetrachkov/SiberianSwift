180656d7cbdca333218009c8510eeba0682e22db
sergey petrachkov <petrachkovsergey@gmail.com>
2017-11-27 20:18:13 +0700

    add app version helpers

--- SiberianSwift/Classes/UIApplication+Siberia.swift
+++ SiberianSwift/Classes/UIApplication+Siberia.swift
@@ -11,12 +11,41 @@ import Foundation
 public extension UIApplication {
   /// Detect if debugger is attached.
   /// In some cases you would want to know that and maybe disable/enable something
-  func amIBeingDebugged() -> Bool {
+  public func amIBeingDebugged() -> Bool {
     var info = kinfo_proc()
     var mib : [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
     var size = MemoryLayout.stride(ofValue: info)
     let junk = sysctl(&mib, UInt32(mib.count), &info, &size, nil, 0)
     assert(junk == 0, "sysctl failed")
     return (info.kp_proc.p_flag & P_TRACED) != 0
+  }
+  
+  public static var appVersion: String {
+    if let appVersion = Bundle.main.object(forInfoDictionaryKey: "CFBundleShortVersionString") {
+      return "\(appVersion)"
+    } else {
+      return ""
+    }
+  }
+  
+  public static var build: String {
+    if let buildVersion = Bundle.main.object(forInfoDictionaryKey: kCFBundleVersionKey as String) {
+      return "\(buildVersion)"
+    } else {
+      return ""
+    }
+  }
+  /// Returns version in v1.0 29 format
+  public static var formattedVersion: String {
+    let version = UIApplication.appVersion
+    let build = UIApplication.build
+    
+    var versionAndBuild = "v\(version)"
+    
+    if version != build {
+      versionAndBuild = "v\(version) (\(build))"
+    }
+    
+    return versionAndBuild
   }
 }
