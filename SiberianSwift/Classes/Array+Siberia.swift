ff64408b46d8a08c4ee2c09c5a04f11b02dcf339
sergey petrachkov <petrachkovsergey@gmail.com>
2018-10-17 14:33:02 +0700

    add array merging extension

---
+++ SiberianSwift/Classes/Array+Siberia.swift
@@ -0,0 +1,15 @@
+//
+//  Array+Siberia.swift
+//  SiberianSwift
+//
+//  Created by Sergey Petrachkov on 17/10/2018.
+//  Copyright © 2018 Sergey Petrachkov. All rights reserved.
+//
+
+import Foundation
+public extension Array where Element : Equatable {
+  public mutating func mergeElements<C : Collection>(newElements: C) where C.Iterator.Element == Element {
+    let filteredList = newElements.filter({!self.contains($0)})
+    self.append(contentsOf: filteredList)
+  }
+}
