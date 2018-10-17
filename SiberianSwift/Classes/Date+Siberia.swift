fb63e79bf32df77c46212d1326749154439b685c
sergey petrachkov <petrachkovsergey@gmail.com>
2018-10-17 14:28:36 +0700

    add helper methods to date extension (calculate number of seconds, minutes, hours, days, months, years since reference date)

--- SiberianSwift/Classes/Date+Siberia.swift
+++ SiberianSwift/Classes/Date+Siberia.swift
@@ -12,4 +12,28 @@ public extension Date {
   func dayNumberOfWeek() -> Int? {
     return Calendar.current.dateComponents([.weekday], from: self).weekday
   }
+  
+  func years(sinceDate: Date) -> Int? {
+    return Calendar.current.dateComponents([.year], from: sinceDate, to: self).year
+  }
+  
+  func months(sinceDate: Date) -> Int? {
+    return Calendar.current.dateComponents([.month], from: sinceDate, to: self).month
+  }
+  
+  func days(sinceDate: Date) -> Int? {
+    return Calendar.current.dateComponents([.day], from: sinceDate, to: self).day
+  }
+  
+  func hours(sinceDate: Date) -> Int? {
+    return Calendar.current.dateComponents([.hour], from: sinceDate, to: self).hour
+  }
+  
+  func minutes(sinceDate: Date) -> Int? {
+    return Calendar.current.dateComponents([.minute], from: sinceDate, to: self).minute
+  }
+  
+  func seconds(sinceDate: Date) -> Int? {
+    return Calendar.current.dateComponents([.second], from: sinceDate, to: self).second
+  }
 }
