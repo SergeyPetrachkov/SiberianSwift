3c1047b2ea4f27238dc9a4f1618458d3adb3d61d
Sergey Petrachkov <petrachkovsergey@gmail.com>
2020-10-30 13:41:40 +0300

    make toast background and font colors configurable + comfigurable positions

--- Sources/SiberianSwift/UIViewController+Siberia.swift
+++ Sources/SiberianSwift/UIViewController+Siberia.swift
@@ -10,13 +10,28 @@
 import UIKit
 
 extension UIViewController {
-  open func showToast(with message: String, width: CGFloat = 300, duration: Double = 4.0, delay: Double = 0.1, fontSize: CGFloat = 14) {
+
+  public enum ToastPosition {
+    case middle
+    case bottom(CGFloat)
+    case top(CGFloat)
+  }
+
+  public func showToast(with message: String,
+                        width: CGFloat = 300,
+                        duration: Double = 4.0,
+                        delay: Double = 0.1,
+                        fontSize: CGFloat = 14,
+                        backgroundColor: UIColor = UIColor.lightGray,
+                        fontColor: UIColor = UIColor.black,
+                        position: ToastPosition = .middle) {
+
     let toastLabel = UILabel(frame: CGRect(x: (self.view.frame.size.width - width) / 2,
                                            y: self.view.frame.size.height / 2,
                                            width: width,
                                            height: 48))
-    toastLabel.backgroundColor = UIColor.lightGray
-    toastLabel.textColor = UIColor.black
+    toastLabel.backgroundColor = backgroundColor
+    toastLabel.textColor = fontColor
     toastLabel.textAlignment = .center
     toastLabel.font = UIFont.systemFont(ofSize: fontSize)
     toastLabel.text = message
@@ -28,6 +43,15 @@ extension UIViewController {
     
     toastLabel.frame.size = CGSize(width: max(size.width, width), height: max(48, size.height))
     toastLabel.center = self.view.center
+
+    switch position {
+    case .middle:
+      toastLabel.center = self.view.center
+    case .bottom(let offset):
+      toastLabel.frame.origin.y = self.view.frame.height - toastLabel.frame.height - offset
+    case .top(let offset):
+      toastLabel.frame.origin.y = offset
+    }
     self.view.addSubview(toastLabel)
     UIView.animate(withDuration: duration,
                    delay: delay,
